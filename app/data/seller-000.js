eb198c8af51a92bc0ceae31bba6bbe8f7e221fbe
lauer3912 <lauer3912@gmail.com>
2017-10-15 21:59:37 +0800

    init

---
+++ app/data/seller-000.js
@@ -0,0 +1,89 @@
+(function(){
+    window["SellerData"] = window["SellerData"] || [];
+    var dataList = {
+        "products":[
+            {
+                "Pname":"Internet Download Manager 国外优秀的下载工具",
+                "Pid":"is4-0454-idm",
+                "Name":"Internet Download Manager",
+                "OS": "win",
+                "OfficalSite":"http://www.internetdownloadmanager.com/",
+                "OfficalPrices":"$24.95",
+                "OfficalProductID":"212127",
+                "Commission": 20,
+                "DownloadUrl":"https://seesrc.com/dl-idman-win",
+                "BuyUrl":"https://order.shareit.com/cart/add?vendorid=40454&PRODUCT[212127]=1&affiliateid=200274083",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=559925904477"
+            },
+            {
+                "Pname":"FolderLock 目录加密工具 Windows版",
+                "Pid":"mc1-2653-folder-lock",
+                "Name":"Folder Lock",
+                "OS": "win",
+                "OfficalSite":"http://www.newsoftwares.net/folderlock",
+                "OfficalPrices":"$39.95",
+                "OfficalProductID":"12653-1",
+                "Commission": 50,
+                "DownloadUrl":"",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/12653-1?affiliate_id=617007",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=559826218000"
+            },
+            {
+                "Pname":"Jhosoft Phone Transfer 手机数据恢复迁移备份助手",
+                "Pid":"mc3-0945-jihosoft-phone-transfer",
+                "Name":"Jhosoft Phone Transfer",
+                "OS": "win",
+                "OfficalSite":"http://www.jihosoft.com/",
+                "OfficalPrices":"$39.95",
+                "OfficalProductID":"30945-54",
+                "Commission": 75,
+                "DownloadUrl":"https://seesrc.com/dl-jiho-pt-win",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/30945-54?affiliate_id=617007",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=5559836481840"
+            },
+            {
+                "Pname":"Flip PDF翻页电子杂志书籍创建编辑工具 Windows版",
+                "Pid":"mc9-891-flip-pdf-for-win",
+                "Name":"Flip PDF for Windows",
+                "OS": "win",
+                "OfficalSite":"http://www.flipbuilder.com/flip-pdf/",
+                "OfficalPrices":"$99",
+                "OfficalProductID":"9891-56",
+                "Commission": 50,
+                "DownloadUrl":"https://seesrc.com/dl-flip-pdf-win",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/9891-56?affiliate_id=617007",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=5559792217265"
+            },
+            {
+                "Pname":"Flip PDF翻页电子杂志书籍创建编辑工具 Mac版",
+                "Pid":"mc9-891-flip-pdf-mac",
+                "Name":"Flip PDF for Mac",
+                "OS": "mac",
+                "OfficalSite":"http://www.flipbuilder.com/flip-pdf/",
+                "OfficalPrices":"$99",
+                "OfficalProductID":"9891-174",
+                "Commission": 51,
+                "DownloadUrl":"https://seesrc.com/dl-flip-pdf-mac",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/9891-174?affiliate_id=617007",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=559946595903"
+            },
+            {
+                "Pname":"MarkdownD markdown文档编辑工具 好用 可视化Win版本",
+                "Pid":"romanysoft-markdownd-win",
+                "Name":"MarkdownD for Windows",
+                "OS": "win",
+                "OfficalSite":"http://www.romanysoft.net",
+                "OfficalPrices":"$19",
+                "OfficalProductID":"",
+                "Commission": 50,
+                "DownloadUrl":"https://seesrc.com/dl-markdownd-win",
+                "BuyUrl":"",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=559830758426"
+            }
+        ]
+    };
+
+    $.each(dataList.products, function(index, ele){
+        window["SellerData"].push(ele);
+    });
+})();
---
+++ app/js/main.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+
+
+    $("#grid").kendoGrid({
+        dataSource: {
+            data: window.SellerData,
+            schema: {
+                model: {
+                    fields: {
+                        "Pname": { type: "string" },
+                        "Pid": { type: "string" },
+                        "Name": { type: "string" },
+                        "OS": { type: "string" },
+                        "OfficalSite": { type: "string" },
+                        "OfficalPrices": { type: "string" },
+                        "OfficalProductID": { type: "string" },
+                        "Commission": { type: "number" },
+                        "DownloadUrl": { type: "string" },
+                        "BuyUrl": { type: "string" },
+                        "TaobaoUrl": { type: "string" },
+                    }
+                }
+            },
+            pageSize: 30
+        },
+        height: $(window).height() - 20,
+        sortable: true,
+        reorderable: true,
+        groupable: true,
+        resizable: true,
+        filterable: true,
+        columnMenu: true,
+        pageable: true,
+        columns: [
+            {
+                field: "Pname",
+                title: "名称",
+                locked: true,
+                lockable: false,
+                format: "{0}",
+                width: 400
+            },{
+                    command:{ 
+                        text: "点击购买", 
+                        click: function(e){
+                            e.preventDefault();
+                            var tr = $(e.target).closest("tr");
+                            var data = this.dataItem(tr);
+                            window.open(data.BuyUrl);
+                        } 
+                    },
+                    title: "操作", 
+                    width: 90,
+                    locked: true,
+                    lockable: false,
+            }, {
+                field: "OS",
+                title: "系统",
+                width: 80
+            },  {
+                field: "Commission",
+                title: "佣金比例",
+                lockable: false,
+                format: "{0}%",
+                width: 120
+            }, {
+                field: "OfficalSite",
+                title: "官方网站",
+                width: 300
+            },{
+                field: "OfficalPrices",
+                title: "官方价格",
+                locked: true,
+                width: 120
+            },  {
+                field: "OfficalProductID",
+                title: "产品ID",
+                lockable: false,
+                width: 150
+            },  {
+                field: "DownloadUrl",
+                title: "试用下载",
+                lockable: false,
+                width: 400
+            },  {
+                field: "BuyUrl",
+                title: "购买授权",
+                lockable: false,
+                width: 400
+            },  {
+                field: "TaobaoUrl",
+                title: "淘宝链接",
+                lockable: false,
+                width: 400
+            }
+        ]
+    });
+});
