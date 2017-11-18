1c9df9e6f50091d6994803aab795c436e1dfa976
lauer3912 <lauer3912@gmail.com>
2017-11-18 21:28:35 +0800

    Add Green SD Card Data Recovery Pro

--- app/data/seller-000.js
+++ app/data/seller-000.js
@@ -290,6 +290,22 @@
                 "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-371?affiliate_id=617007",
                 "TaobaoUrl":"https://item.taobao.com/item.htm?id=560381232686",
                 "TaobaoPrice": 669    
+            },
+            {
+                "Pname":"原子正版 Green SD Card Data Recovery Pro SD存储卡恢复 Win",
+                "Pid":"mc5-BestDataRecovery-7320-green-sd-card-data-recovery-pro-win",
+                "Name":"Green SD Card Data Recovery Pro for Win",
+                "OS": "win",
+                "site":"http://best-datarecovery.com/sd-card-data-recovery-pro/",
+                "orgPrice":39.99,
+                "sitePrice":39.99,
+                "productID":"10",
+                "Commission": 70,
+                "OrgDownloadUrl":"http://hotdownloads.com/trialware/download/Download_green_sd_card_data_recovery.exe?item=57320-10&affiliate=617007",
+                "DownloadUrl":"https://seesrc.com/dl-green-sd-card-data-recovery-pro-win",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/57320-10?affiliate_id=617007",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=561583365779",
+                "TaobaoPrice": 299    
             }
         ]
     };
