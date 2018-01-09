640b42ca8b6984215271dab7c25cf62f9aa2cee0
iansun <lauer3912@gmail.com>
2018-01-09 16:59:29 +0800

    Wondershare 更新一部分

--- app/data/mycommerce/mc8799-wondershare.js
+++ app/data/mycommerce/mc8799-wondershare.js
@@ -12,7 +12,8 @@
                 "sitePrice":49.95,
                 "productID":"8799-211",
                 "Commission": 50,
-                "DownloadUrl":"https://seesrc.com/dl-ws-data-recovery-win",
+                "DownloadUrl":"https://pan.baidu.com/s/1nw9Cjf7",
+                "OrgDownloadUrl":"http://download.wondershare.com/data-recovery_full542.exe",
                 "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-211?affiliate_id=617007",
                 "TaobaoUrl":"https://item.taobao.com/item.htm?id=559992808327"
             },
@@ -26,40 +27,105 @@
                 "sitePrice":95.95,
                 "productID":"8799-263",
                 "Commission": 50,
-                "DownloadUrl":"http://seesrc.com/dl-ws-data-recovery-mac",
+                "DownloadUrl":"https://pan.baidu.com/s/1c31MYHU",
+                "OrgDownloadUrl":"http://download.wondershare.com/mac-data-recovery_full737.dmg",
                 "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-263?affiliate_id=617007",
                 "TaobaoUrl":"https://item.taobao.com/item.htm?id=560086833336"
             },
+
+            // dr.fone-full - for ios
             {
-                "Pname":"Wondershare dr.fone toolkit iOS Data Recovery Lifetime授权",
+                "Pname":"Wondershare dr.fone toolkit iOS Suite Lifetime授权 Win",
+                "Pid":"mc8-799-dr.fone-toolkit-ios-suite-win",
+                "Name":"Wondershare dr.fone toolkit - iOS Suite for Win",
+                "OS": "win",
+                "site":"https://drfone.wondershare.com/mac-iphone-data-recovery.html",
+                "orgPrice":99.95, // 996
+                "sitePrice":99.95,
+                "productID":"8799-405",
+                "Commission": 50,
+                "DownloadUrl":"https://seesrc.com/dl-ws-dr.fone-toolkit-ios-suite-win",
+                "OrgDownloadUrl":"http://download.wondershare.com/drfone_full3360.exe",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-405?affiliate_id=617007",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=560419993067"
+            },
+            // TODO: 需要提供Mac版本
+            /**
+            {
+                "Pname":"Wondershare dr.fone toolkit iOS Suite Lifetime授权 Mac",
+                "Pid":"mc8-799-dr.fone-toolkit-ios-suite-mac",
+                "Name":"Wondershare dr.fone toolkit - iOS Suite for Mac",
+                "OS": "mac",
+                "site":"https://drfone.wondershare.com/mac-iphone-data-recovery.html",
+                "orgPrice":139.95,  // 1016
+                "sitePrice":139.95,
+                "productID":"8799-402",
+                "Commission": 50,
+                "DownloadUrl":"https://seesrc.com/dl-ws-dr.fone-toolkit-ios-suite-win",
+                "OrgDownloadUrl":"http://download.wondershare.com/drfone_full3361.dmg",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-402?affiliate_id=617007",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=560419993067"
+            },
+             */
+
+
+
+
+            //dr.fone-full - for android
+            {
+                "Pname":"Dr.Fone-Android Full Suite安卓全套工具 终生授权 Win版本",
+                "Pid":"mc8-799-dr.fone-toolkit-android-data-recovery-win",
+                "Name":"Wondershare dr.fone toolkit - Android Full Suite for Win",
+                "OS": "win",
+                "site":"https://drfone.wondershare.com/android-data-recovery.html",
+                "orgPrice":119.95,
+                "sitePrice":119.95,
+                "productID":"8799-371",
+                "Commission": 50,
+                "OrgDownloadUrl":"http://hotdownloads.com/trialware/download/Download_drfone-for-android_setup_full1464.exe?item=8799-371&affiliate=617007",
+                "DownloadUrl":"https://seesrc.com/dl-ws-dr.fone-toolkit-android-full-suite-win",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-371?affiliate_id=617007",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=560381232686"
+            },
+
+
+            //dr.fone-data-recovery
+            {
+                "Pname":"Wondershare dr.fone iOS Data Recovery Lifetime授权 Win",
                 "Pid":"mc8-799-dr.fone-toolkit-ios-data-recovery-win",
-                "Name":"Wondershare dr.fone toolkit - iOS Data Recovery for Win",
+                "Name":"Wondershare dr.fone-iOS Data Recovery for Win",
                 "OS": "win",
                 "site":"https://drfone.wondershare.com/mac-iphone-data-recovery.html",
                 "orgPrice":69.95,
                 "sitePrice":69.95,
                 "productID":"8799-421",
                 "Commission": 50,
-                "OrgDownloadUrl":"http://hotdownloads.com/trialware/download/Download_win-drfone_setup_full3360.exe?item=8799-421&affiliate=617007",
-                "DownloadUrl":"https://seesrc.com/dl-ws-dr.fone-toolkit-ios-data-recovery-win",
+                "OrgDownloadUrl":"",
+                "DownloadUrl":"https://pan.baidu.com/s/1smasihn",
+                "OrgDownloadUrl":"http://download.wondershare.com/drfone_recover_full3366.exe",
                 "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-421?affiliate_id=617007",
                 "TaobaoUrl":"https://item.taobao.com/item.htm?id=560481734544"
             },
+            /***
+            TODO: 这里缺少Mac版本，后面需要在淘宝上添加
             {
-                "Pname":"Wondershare dr.fone toolkit iOS Suite Lifetime授权",
-                "Pid":"mc8-799-dr.fone-toolkit-ios-suite-win",
-                "Name":"Wondershare dr.fone toolkit - iOS Suite for Win",
-                "OS": "win",
+                "Pname":"Wondershare dr.fone iOS Data Recovery Lifetime授权 Mac",
+                "Pid":"mc8-799-dr.fone-toolkit-ios-data-recovery-mac",
+                "Name":"Wondershare dr.fone-iOS Data Recovery for Mac",
+                "OS": "mac",
                 "site":"https://drfone.wondershare.com/mac-iphone-data-recovery.html",
-                "orgPrice":139.95,
-                "sitePrice":139.95,
-                "productID":"8799-321",
+                "orgPrice":79.95,
+                "sitePrice":79.95,
+                "productID":"8799-426",
                 "Commission": 50,
-                "OrgDownloadUrl":"http://hotdownloads.com/trialware/download/Download_drfone-for-ios_setup_full1283.exe?item=8799-321&affiliate=617007",
-                "DownloadUrl":"https://seesrc.com/dl-ws-dr.fone-toolkit-ios-suite-win",
-                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-321?affiliate_id=617007",
-                "TaobaoUrl":"https://item.taobao.com/item.htm?id=560419993067" 
+                "OrgDownloadUrl":"",
+                "DownloadUrl":"https://pan.baidu.com/s/1bqeOz1t",
+                "OrgDownloadUrl":"http://download.wondershare.com/drfone_recover_full3353.dmg",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-426?affiliate_id=617007",
+                "TaobaoUrl":""
             },
+
+             */
             {
                 "Pname":"Dr.Fone-安卓数据恢复工具 终生授权",
                 "Pid":"mc8-799-dr.fone-toolkit-android-data-recovery-win",
@@ -74,21 +140,6 @@
                 "DownloadUrl":"https://seesrc.com/dl-ws-dr.fone-toolkit-android-data-recovery-win",
                 "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-368?affiliate_id=617007",
                 "TaobaoUrl":"https://item.taobao.com/item.htm?id=560537566711"  
-            },
-            {
-                "Pname":"Dr.Fone-Android Full Suite安卓全套工具 终生授权",
-                "Pid":"mc8-799-dr.fone-toolkit-android-data-recovery-win",
-                "Name":"Wondershare dr.fone toolkit - Android Full Suite for Win",
-                "OS": "win",
-                "site":"https://drfone.wondershare.com/android-data-recovery.html",
-                "orgPrice":119.95,
-                "sitePrice":119.95,
-                "productID":"8799-371",
-                "Commission": 50,
-                "OrgDownloadUrl":"http://hotdownloads.com/trialware/download/Download_drfone-for-android_setup_full1464.exe?item=8799-371&affiliate=617007",
-                "DownloadUrl":"https://seesrc.com/dl-ws-dr.fone-toolkit-android-full-suite-win",
-                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/8799-371?affiliate_id=617007",
-                "TaobaoUrl":"https://item.taobao.com/item.htm?id=560381232686"   
             },
             /// dr.fone - Repair (iOS System Recovery)
             /// 中文网站：https://drfone.wondershare.com/zh/ios-system-recovery.html
