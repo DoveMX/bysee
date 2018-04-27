ae7da8f1a4a164d51ccd5f52c085f08626a6ee0c
Ian <Ian@seesrc.com>
2018-04-27 10:27:47 +0800

    save all

--- app/data/mycommerce/mc5388-stellarinfo.js
+++ app/data/mycommerce/mc5388-stellarinfo.js
@@ -17,6 +17,24 @@
                 "OrgDownloadUrl":"http://download.stellarinfo.com/StellarPhoenixWindowsDataRecovery-Professional.exe",
                 "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/5388-52?affiliate_id=624209",
                 "TaobaoUrl":"https://item.taobao.com/item.htm?id=568740883832"
+            },
+
+            /// Stellar Phoenix Video Repair Tool
+            {
+                "Pname":"原子正版 Stellar Phoenix Video Repair Tool 视频恢复工具 Windows版本",
+                "Pid":"mc5388-stellarinfo-StellarPhoenixVideoRepairTool_-win",
+                "Name":"Stellar Phoenix Video Repair Tool - Windows",
+                "OS": "win",
+                "site":"https://www.stellarinfo.com/disk-recovery/video-repair.php",
+                "orgPrice":69.99,
+                "sitePrice":69.99,
+                "productID":"5388-139",
+                "Commission": 30,
+                "dlID":"dl-stellarphoenixvideorepairtool-win",
+                "DownloadUrl":"https://seesrc.com/dm/www.stellarinfo.com/video-repair/StellarPhoenixVideoRepair.exe",
+                "OrgDownloadUrl":"http://download.stellarinfo.com/StellarPhoenixVideoRepair.exe",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/5388-139?affiliate_id=624209",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=568502736127"
             }
         ]
     };
