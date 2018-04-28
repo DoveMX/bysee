cc4f3b3fe55f09678c6ebaf66674543f8c6d07f7
Ian <Ian@seesrc.com>
2018-04-28 10:25:35 +0800

    save all

--- app/data/mycommerce/mc5388-stellarinfo.js
+++ app/data/mycommerce/mc5388-stellarinfo.js
@@ -1,7 +1,18 @@
 (function(){
     window["SellerData"] = window["SellerData"] || [];
+// https://www.stellarinfo.com/windows-data-recovery-professional.php
+// https://www.stellarinfo.com/data-recovery-mac.php
+// https://www.stellarinfo.com/digital-media-recovery.php
+// https://www.stellarinfo.com/disk-recovery/video-repair.php
+// https://www.stellarinfo.com/convert-ost-to-pst.php
+// https://www.stellarinfo.com/edb-exchange-server-recovery.htm
+// https://www.stellarinfo.com/outlook-pst-file-recovery.php
+// https://www.stellarinfo.com/sql-recovery.php
+// https://www.stellarinfo.com/access-database-repair.php
+// https://www.stellarinfo.com/email-tools/exchange-toolkit.php
     var dataList = {
         "products":[
+            /// Stellar Phoenix Windows Data Recovery
             {
                 "Pname":"原子正版 Stellar Phoenix Windows Data Recovery 数据恢复工具",
                 "Pid":"mc5388-stellarinfo-StellarPhoenixWindowsDataRecovery–Professiona_-win",
@@ -19,6 +30,10 @@
                 "TaobaoUrl":"https://item.taobao.com/item.htm?id=568740883832"
             },
 
+            /// Stellar Phoenix Mac Data Recovery
+            //TODO: Stellar Phoenix Mac Data Recovery
+
+
             /// Stellar Phoenix Video Repair Tool
             {
                 "Pname":"原子正版 Stellar Phoenix Video Repair Tool 视频恢复工具 Windows版本",
@@ -35,7 +50,26 @@
                 "OrgDownloadUrl":"http://download.stellarinfo.com/StellarPhoenixVideoRepair.exe",
                 "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/5388-139?affiliate_id=624209",
                 "TaobaoUrl":"https://item.taobao.com/item.htm?id=568502736127"
+            },
+            {
+                "Pname":"原子正版 Stellar Phoenix Video Repair Tool 视频恢复工具 Mac版本",
+                "Pid":"mc5388-stellarinfo-StellarPhoenixVideoRepairTool_-mac",
+                "Name":"Stellar Phoenix Video Repair Tool - Mac",
+                "OS": "mac",
+                "site":"https://www.stellarinfo.com/disk-recovery/video-repair.php",
+                "orgPrice":69.99,
+                "sitePrice":69.99,
+                "productID":"5388-112",
+                "Commission": 30,
+                "dlID":"dl-stellarphoenixvideorepairtool-mac",
+                "DownloadUrl":"https://seesrc.com/dm/www.stellarinfo.com/video-repair/StellarPhoenixVideoRepair.dmg.zip",
+                "OrgDownloadUrl":"http://download.stellarinfo.com/StellarPhoenixVideoRepair.dmg.zip",
+                "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/add/5388-112?affiliate_id=624209",
+                "TaobaoUrl":"https://item.taobao.com/item.htm?id=568784766154"
             }
+
+
+
         ]
     };
 
