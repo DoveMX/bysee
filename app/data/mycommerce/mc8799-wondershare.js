6aaa9721ce28fb92f17f42f89f89d56acfc0a60b
Ian <Ian@techidaily.com>
2018-07-17 23:48:42 -0400

    发布 原子正版 Dr.fone iPhone手机数据恢复工具全套装 Mac版本

--- app/data/mycommerce/mc8799-wondershare.js
+++ app/data/mycommerce/mc8799-wondershare.js
@@ -267,7 +267,7 @@
                     "dlID":"dl-ws-dr.fone-ios-toolkit-mac",
                     "DownloadUrl":"https://romanysoft.com/files/wondershare/drfone_full.dmg",
                     "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/new/8799-402?quantity=1&locale=zh_CN&currency=USD&affiliate_id=624209&affiliate=624209",
-                    "TaobaoUrl":""
+                    "TaobaoUrl":"https://item.taobao.com/item.htm?id=573992447675"
                 }
             ],
 
