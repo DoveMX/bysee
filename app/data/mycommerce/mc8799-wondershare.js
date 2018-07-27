03a619c5996364181d852b6d83003e5faa09cc5f
Ian <Ian@techidaily.com>
2018-07-27 15:03:17 +0800

    发布 原子正版 Dr.Fone Android 安卓手机数据传输工具 Windows版本

--- app/data/mycommerce/mc8799-wondershare.js
+++ app/data/mycommerce/mc8799-wondershare.js
@@ -325,7 +325,7 @@
                     "dlID":"dl-ws-dr.fone-android-transfer-win",
                     "DownloadUrl":"https://romanysoft.com/files/wondershare/drfone_transfer_setup_full.exe",
                     "BuyUrl":"https://shopper.mycommerce.com/checkout/cart/new/8799-408?quantity=1&locale=zh_CN&currency=USD&affiliate_id=624209&affiliate=624209",
-                    "TaobaoUrl":""
+                    "TaobaoUrl":"https://item.taobao.com/item.htm?id=574327345329"
                 }
                 ,{
                     "SName":"Android Transfer Tool",
